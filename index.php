d7c7b610ef3c63ea35e55affd4ec71cc50aeee6f
Volkan Sunguray <volkan@sunguray.com>
2022-06-13 21:27:34 +0300

    çalışmalar yüklendi

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    include("header.php");
+?>
+
+<?php
+
+    // 1. Soru cevabı    
+
+    $yas = rand(0, 50);
+    if ($yas < 18 ) { 
+    echo "Yaşınız Ehliyet Almaya Yetmiyor: $yas";
+    }
+    elseif ($yas >= 18) {  
+    echo "Yaşınız Ehliyet Almaya Yetiyor: $yas";
+    }
+
+    // 2. Soru cevabı
+
+    try {
+		$db = new PDO("mysql:host=localhost;dbname=veresiye;charset=UTF8", "root", "123");
+		echo "</br></br>Bağlantı Başarılı...";
+    }
+    catch(PDOException $e){
+        print $e->getMessage();
+    }
+
+    // 3.Soru cevabı
+
+    // 1-3 ve 30-32. satırlar
+
+?>
+    
+<?php
+    include("foother.php");
+?>
